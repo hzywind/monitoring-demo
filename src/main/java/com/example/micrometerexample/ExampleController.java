565eddc3e775cbd8685f619f6c4c47b0893538d8
Wind Hong <hzywind@gmail.com>
2023-04-28 18:18:09 +0800

    update controller

--- src/main/java/com/example/micrometerexample/ExampleController.java
+++ src/main/java/com/example/micrometerexample/ExampleController.java
@@ -1,43 +1,39 @@
 package com.example.micrometerexample;
 
-import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
-import java.util.Random;
+import java.util.Date;
 
 @RestController
 public class ExampleController {
-    private final DistributionSummary summary;
     private final Timer timer;
     public ExampleController(MeterRegistry registry) {
-         summary = DistributionSummary
-                 .builder("controller.summary")
-                 .baseUnit("ms")
-                 .maximumExpectedValue(50d)
-                 .minimumExpectedValue(10d)
-                 .publishPercentileHistogram()
-                 .publishPercentiles(0.5, 0.99)
-                 .serviceLevelObjectives(20)
-                 .register(registry);
          timer = Timer
                  .builder("controller.timer")
-                 .maximumExpectedValue(Duration.ofMillis(5))
+                 .minimumExpectedValue(Duration.ofMillis(30))
+                 .maximumExpectedValue(Duration.ofMillis(500))
                  .publishPercentileHistogram()
                  .publishPercentiles(0.5, 0.99)
-                 .serviceLevelObjectives(Duration.ofMillis(2))
+                 .serviceLevelObjectives(Duration.ofMillis(100))
                  .register(registry);
     }
     @GetMapping("/latency")
-    public String endpoint() throws Exception {
+    public String latency() throws Exception {
         return "latency:" + timer.recordCallable(() -> {
-            int latency = 10 + new Random().nextInt(50);
-            summary.record(latency);
-            return latency;
+            Date start = new Date();
+            URL url = new URL("http://www.bing.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            connection.disconnect();
+            Date end = new Date();
+            return end.getTime() - start.getTime();
         });
     }
-
 }
